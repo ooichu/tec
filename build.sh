fbb3bd2ed255712445288e7e53b0ce46ebf79233
ooichu <nikitapokrovsky5@gmail.com>
2023-12-08 01:14:01 +0300

    Updated elis

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ELIS="elis/elis.c -DELIS_PAGE_SIZE=16384 -DELIS_STACK_SIZE=1024"
 SDL2=`sdl2-config --libs --cflags`
+
 gcc tec.c $ELIS $SDL2 -lm -o tec -O3 -Wall -Wextra -pedantic -std=c99 
 strip tec
